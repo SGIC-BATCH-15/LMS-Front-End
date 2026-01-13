4b3e1c7cbe8a30ee65c3a0434cf206c5d886fe2c
Vithu_Selva <88456626+selvaratnamvithusha@users.noreply.github.com>
2026-01-13 14:15:51 +0530

    Revert "De company 6-suruthi"

--- src/data/holidays.ts
+++
@@ -1,7 +0,0 @@
-import { Holiday } from '@/types';
-
-// Mock initial data
-export const initialHolidays: Holiday[] = [
-    { id: '1', name: 'New Year', date: new Date(2024, 0, 1), companyId: '1', type: 'public' },
-    { id: '2', name: 'Christmas', date: new Date(2024, 11, 25), companyId: '1', type: 'public' },
-];
